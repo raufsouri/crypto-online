ac101245ea469668da705cddf44481bbe8cd4450
rauf souri <sourirauf@gmail.com>
2022-06-04 20:41:14 +0430

    added files

---
+++ crypto-online/src/component/Landing.js
@@ -0,0 +1,54 @@
+import React , {useEffect , useState } from 'react';
+import { getCoin } from '../services/api';
+import Loader from './Loader';
+import Coin from './Coin';
+import styles from './Landing.module.css'
+const Landing = () => {
+
+    const [coins, setCoins] = useState([]);
+    const [search , setSearch] = useState("");
+ 
+useEffect(() =>{
+
+const fetchAPI = async () => {
+    const data = await getCoin();
+    console.log(data);
+    setCoins(data)
+}
+      fetchAPI();
+},[])
+    
+
+    const searchHandler = event => {
+        setSearch(event.target.value);
+    }
+
+    const searchedCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
+
+    return (
+     <>
+   
+     <input className={styles.input} type="text" placeholder="search" value={search} onChange = {searchHandler} />
+     {
+                coins.length ?
+                    <div  className={styles.container}>
+                        {
+                           searchedCoins.map(coin => <Coin
+                                key={coin.id}
+                                name={coin.name}
+                                image={coin.image}
+                                symbol={coin.symbol}
+                                price={coin.current_price}
+                                marketCap={coin.market_cap}
+                                priceChange={coin.price_change_percentage_24h}
+                            />)
+                        }
+                    </div> :
+
+                    <Loader />
+            }
+     </>
+    );
+};
+
+export default Landing;
